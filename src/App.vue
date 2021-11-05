f0e53ebc28395e75bc83b058737a101e449f8bcd
Alexander Tomashov <sanya-81-chess@mail.ru>
2021-11-05 10:56:55 +0800

    feat: normalaze and baseButton.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,71 @@
 <template>
 <div>
-  <p>start</p>
+    <p><label><input type="radio" value="one" v-model="arr">1</label></p>	
+    <p><label><input type="radio" value="two" v-model="arr">2</label></p>	
+    <p>{{arr}}</p>
+
+    <select v-model='selected'>
+        <option  
+            v-for="option in options" 
+            :key="option.value"
+            :value='option.value'
+        >
+            {{option.text}}
+        </option>
+    </select>
+    <p>{{selected}}</p>
+    <p><label><input type="radio" name='of' v-model='select' :value='a'></label></p>
+    <p><label><input type="radio" name='of' v-model='select' :value='a'></label></p>
+    <!-- <p><label><input  v-model.lazy='select'></label></p> -->
+    <p><label><input type='number'  v-model="select"></label></p>
+    <p>{{select}}</p>
+    <baseButton></baseButton>
 </div>
 </template>
 
 <script>
-
+import baseButton from './components/baseButton.vue'
 export default {
-  name: 'App',
+  components: { baseButton },
+    data() {
+        return {
+            name: 'App',
+            select: 0,
+            a: true,
+            selected: 'A',
+             options: [
+                { text: 'Один', value: 'A' },
+                { text: 'Два', value: 'B' },
+                { text: 'Три', value: 'C' }
+            ]
+        }
+    }
 }
 </script>
 
 <style>
+    :root {
+    --blue50: #EBF4F8;
+    --blue100: #DBE2EA;
+    --blue600: #0880AE;
+    --blue900: #2C2738;
+    --black900: rgba(0, 0, 0, 0.25);
+    --black800: rgba(44, 39, 56, 0.08);
+
+    --dropShadow: drop-shadow(0px 4px 4px var(--black900));
+    --textShadow: 0px 4px 4px var(--black900);
+    --shadowNormal: 
+        0px 2px 4px var(--black800), 
+        0px 4px 8px var(--black800);
+    --shadowHover: 
+        0px 12px 24px var(--black800),
+        0px 24px 48px rgba(44, 39, 56, 0.16);
+    --shadowActive: 
+        0px 2px 4px rgba(44, 39, 56, 0.0001), 
+        0px 4px 8px var(--black800);
+        
+    }
+    @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@500&display=swap');
 </style>
+
+
---
+++ src/components/baseButton.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+    <button>Button</button>
+    <button class="button">Button</button>
+</div>
+</template>
+
+<style lang="scss" scoped>
+    
+    button {
+        appearance: none;
+        border: 0;
+        border-radius: 0;
+        background: #fff;
+    }
+
+    .button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        
+        width:  180px;
+        height: 56px;
+        color: var(--blue50);
+        background-color: var(--blue600);
+        border-radius: 6px;
+        box-shadow: var(--shadowNormal);
+        font-family: 'IBM Plex Sans', sans-serif;
+        text-shadow: var(--textShadow);
+        
+        &:hover {
+            text-shadow: none;
+            box-shadow: var(--shadowHover);    
+            filter: var(--dropShadow)
+        }
+
+        &:active {
+            border: 2px solid var(--blue900);
+            box-shadow: var(--shadowActive);    
+        }
+    }
+</style>
